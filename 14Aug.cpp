31f7d26c7884c29bdfb2a9b5d6b17bf7bcaed80e
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-08-14 01:40:04 +0530

    14 august POTD

--- 14Aug.cpp
+++ 14Aug.cpp
@@ -1,65 +1,27 @@
-int n1 = nums1.size();
-        int n2 = nums2.size();  
-        int total_elements = n1 + n2;
-        //I always want to perform binary search on shorter array..
-        //I will always perform on nums1 and so if second array size is
-        //larger, swap the two arrays..
-        if(n1 > n2)
+vector<int> singleNumber(vector<int> nums) 
+    {
+        int n = nums.size();
+        int xr = nums[0];
+        for(int i=1;i<n;i++)
         {
-            return findMedianSortedArrays(nums2,nums1);
+            xr = xr ^ nums[i]; 
         }
-        //I require leftreq elements on left side..
-        int leftreq = (n1 + n2 + 1)/2;
-        int start = 0;
-        int end = n1;
-        while(start<=end)
+        int right_bit = xr & ~(xr - 1);
+        int x=0;
+        int y=0;
+        for(int i=0;i<n;i++)
         {
-            //mid1 elements on left and mid2 elements on right...
-            int mid1 = start + (end-start)/2;
-            int mid2 = leftreq - mid1;
-            int l1 = INT_MIN;
-            int l2 = INT_MIN;
-            int r1 = INT_MAX;
-            int r2 = INT_MAX;
-            //check validity for all 4 pointers...
-            if(mid1 < n1)
+            if(nums[i]&right_bit)
             {
-                r1 = nums1[mid1];
-            }
-            if(mid2 < n2)
-            {
-                r2 = nums2[mid2];
-            }
-            if(mid1 - 1 >= 0)
-            {
-                l1 = nums1[mid1-1];
-            }
-            if(mid2 - 1 >= 0)
-            {
-                l2 = nums2[mid2 - 1];
-            }
-            //if our sequence is valid..
-            if(l1 <= r2 && l2 <= r1)
-            {
-                //if total number of elements are odd, return max of both lefts..
-                if(total_elements % 2)
-                {
-                    return max(l1,l2);
-                }
-                //else return max of lefts + min of rights divided by 2..
-                else
-                {
-                    return ((double)(max(l1,l2) + min(r1,r2))) / 2.0;   
-                }
-            }
-            //derived by dry run..
-            else if(l1 > r2)
-            {
-                end = mid1 - 1;
+                x = x ^ nums[i];
             }
             else
             {
-                start = mid1 + 1;
+                y = y ^ nums[i];
             }
         }
-        return 0;
+        vector<int>ans;
+        ans.push_back(min(x,y));
+        ans.push_back(max(x,y));
+        return ans;
+    }
