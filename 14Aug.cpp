db08b2a10a5da1da372c81bd5ba9d920e75f7ed0
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-08-14 20:06:21 +0530

    14 Aug GFG POTD

--- 14Aug.cpp
+++ 14Aug.cpp
@@ -1,27 +1,27 @@
-vector<int> singleNumber(vector<int> nums) 
+vector<int> singleNumber(vector<int> nums)
+{
+    int n = nums.size();
+    int xr = nums[0];
+    for (int i = 1; i < n; i++)
     {
-        int n = nums.size();
-        int xr = nums[0];
-        for(int i=1;i<n;i++)
-        {
-            xr = xr ^ nums[i]; 
-        }
-        int right_bit = xr & ~(xr - 1);
-        int x=0;
-        int y=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]&right_bit)
-            {
-                x = x ^ nums[i];
-            }
-            else
-            {
-                y = y ^ nums[i];
-            }
-        }
-        vector<int>ans;
-        ans.push_back(min(x,y));
-        ans.push_back(max(x,y));
-        return ans;
+        xr = xr ^ nums[i];
     }
+    int right_bit = xr & ~(xr - 1);
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] & right_bit)
+        {
+            x = x ^ nums[i];
+        }
+        else
+        {
+            y = y ^ nums[i];
+        }
+    }
+    vector<int> ans;
+    ans.push_back(min(x, y));
+    ans.push_back(max(x, y));
+    return ans;
+}
