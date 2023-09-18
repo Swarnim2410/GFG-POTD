f1a8834f46aacbabd6019439ae3570b5f6a216b5
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-09-18 19:10:33 +0530

    18 Sept GFG POTD

--- 18Sept.cpp
+++ 18Sept.cpp
@@ -1,12 +1,9 @@
-class Solution{
-    public:
+class Solution
+{
+public:
     // Function to check if given number n is a power of two.
-    bool isPowerofTwo(long long n){
-        
-        // Your code here   
-        int x = __builtin_popcountll(n);
-        //cout << x;
-        return x==1;
-        
+    bool isPowerofTwo(long long n)
+    {
+        return __builtin_popcountll(n) == 1;
     }
 };
