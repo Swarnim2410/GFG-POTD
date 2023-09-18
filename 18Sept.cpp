bdab9d8cbb13932b35e2906a2a816be8a9e6b55e
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-09-18 19:14:10 +0530

    18 Sept GFG POTD

--- 18Sept.cpp
+++ 18Sept.cpp
@@ -5,5 +5,18 @@ public:
     bool isPowerofTwo(long long n)
     {
         return __builtin_popcountll(n) == 1;
+        /*if(n==0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        if(n%2)
+        {
+            return false;
+        }
+        return isPowerofTwo(n/2);*/
     }
 };
