0b3170b5e45c681446406ec395694771583598cb
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-10-15 00:06:39 +0530

    13 Oct GFG POTD

--- 13Oct.cpp
+++ 13Oct.cpp
@@ -1,56 +1,57 @@
-class Solution{
+class Solution
+{
 
 public:
-    int floor(Node* root, int x) 
+    // MORRIS INORDER TRAVERSAL..
+    int floor(Node *root, int x)
     {
         Node *pre;
         int maxi = -1;
         Node *curr = root;
-        while(curr)
+        while (curr)
         {
             int count = 0;
             pre = NULL;
-            if(curr->left==NULL)
+            if (curr->left == NULL)
             {
                 int val = curr->data;
-                if(curr->data==x)
+                if (curr->data == x)
                 {
                     return x;
                 }
-                if(curr->data<x)
+                if (curr->data < x)
                 {
-                    maxi = max(maxi,curr->data);
+                    maxi = max(maxi, curr->data);
                 }
-                curr = curr ->right;
+                curr = curr->right;
             }
             else
             {
                 pre = curr->left;
-                while(pre->right && pre->right!=curr)
+                while (pre->right && pre->right != curr)
                 {
                     pre = pre->right;
                 }
-                if(pre->right==NULL)
+                if (pre->right == NULL)
                 {
-                    pre ->right = curr;
-                    curr=curr->left;
+                    pre->right = curr;
+                    curr = curr->left;
                 }
                 else
                 {
-                    pre->right=NULL;
+                    pre->right = NULL;
                     int val = curr->data;
-                    if(curr->data==x)
+                    if (curr->data == x)
                     {
                         return x;
                     }
-                    if(curr->data<x)
+                    if (curr->data < x)
                     {
-                        maxi = max(maxi,curr->data);
+                        maxi = max(maxi, curr->data);
                     }
-                    curr=curr->right;
+                    curr = curr->right;
                 }
             }
-            
         }
         return maxi;
     }
